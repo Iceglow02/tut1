3b29d3edb0ca313a95d36d683a9997d314e682e6
Mishke van Wyk <mishkevw@gmail.com>
2023-07-30 19:08:54 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,7 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    int bug = 1; //Bug fix
     String language = args[0];
     String country = args[1];
     String name = args[2];
